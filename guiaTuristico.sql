a33839173b6432cfc3bc757e454ed76e2c5353c2
dudagii <eduarda.giamlourenco@aluno.ifsp.edu.br>
2026-03-13 09:26:09 -0300

    Update guiaTuristico.sql

--- guiaTuristico.sql
+++ guiaTuristico.sql
@@ -55,6 +55,3 @@ CREATE TABLE parada_roteiro (
     FOREIGN KEY (id_roteiro) REFERENCES roteiro(id_roteiro)
 );
 
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4400.153632494484!2d-47.88077875768632!3d-21.97014521486421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b8711e71fe250f%3A0xf5c8879e76e658c4!2sInstituto%20Federal%20de%20Educa%C3%A7%C3%A3o%2C%20Ci%C3%AAncia%20e%20Tecnologia%20de%20S%C3%A3o%20Paulo%2C%20Campus%20S%C3%A3o%20Carlos!5e0!3m2!1spt-BR!2sbr!4v1773414230147!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
