ac6869f4118df5f971e6e0a5b94bf3548f461c8e
emilymiglior <emily.m@aluno.ifsp.edu.br>
2026-04-10 20:47:45 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const conexao = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "1234",
+    password: "ifsp",
     database: "guiaTuristico",
     port: 3306
 });
