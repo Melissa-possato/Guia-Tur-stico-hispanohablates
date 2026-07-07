3600cde847cf7eedea766006fe468be846566ff7
mpossato <mepossato@gmail.com>
2026-07-07 17:31:15 -0300

    Update LoginUsuario.jsx

--- frontend/src/componentes/LoginUsuario.jsx
+++ frontend/src/componentes/LoginUsuario.jsx
@@ -2,21 +2,26 @@ import { useState } from "react";
 
 export default function LoginUsuario({ onLogin }) {
 
-  const [nome, setNome] = useState("");
+   const [nomeUsuario, setNomeUsuario] = useState("");
   const [senha, setSenha] = useState("");
-
+  const [email, setEmail] = useState("");
+  const[telefone, setTelefone] = useState("");
+  const [paisOrigem, setPaisOrigem] = useState("");
   const handleSubmit = (e) => {
 
     e.preventDefault();
 
-    if (!nome || !senha) {
+    if (!nome || !senha || !email || !telefone || !paisOrigem) {
       alert("Preencha todos os campos");
       return;
     }
 
     onLogin({
-      nome_usuario: nome,
-      senha: senha
+        nome_usuario: nomeUsuario,
+        telefone: telefone,
+        email: email,
+        paisOrigem: paisOrigem,
+        senha: senha
     });
 
   };
@@ -27,19 +32,44 @@ export default function LoginUsuario({ onLogin }) {
 
       <h3>Login</h3>
 
-      <input
-        type="text"
-        placeholder="Nome do usuário"
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
-      />
-
-      <input
-        type="password"
-        placeholder="Senha"
-        value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-      />
+       <input
+              type="text"
+              placeholder="Nombre de usuario"
+              value={nomeUsuario}
+              onChange={(e) => setNomeUsuario(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="teléfono"
+              value={String}
+              onChange={(e) => setTelefone(e.target.value)}
+              required
+            />
+  
+            <input
+              type="email"
+              placeholder="País natal"
+              value={String}
+              onChange={(e) => setPaisOrigem(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Correo electrónico"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            
+  
+            <input
+              type="password"
+              placeholder="Contraseña"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
 
       <button type="submit">
         Entrar
